ee577a00b88f770fbcd4843bc03c81f0b4c3855b
AsiaEasa <asiaeasa89@gmail.com>
2024-07-07 22:17:01 -0700

    done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module to calculate the minimum number of operations"""
+
+def minOperations(i):
+    """Calculate the fewest number of operations"""
+    if i <= 1:
+        return 0
+    Op = 0
+    Fa = 2
+    while i > 1:
+        while i % Fa == 0:
+            Op += Fa
+            i //= Fa
+        Fa += 1
+    return Op
