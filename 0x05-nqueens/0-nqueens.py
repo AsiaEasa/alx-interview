986da64ffca26e9643c0ee8f71489af37d850813
AsiaEasa <asiaeasa89@gmail.com>
2024-07-30 12:10:48 -0700

    done

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -7,50 +7,56 @@ def USAGE_ERROR():
     print("Usage: nqueens N")
     sys.exit(1)
 
+
 def NUMBER_ERROR():
     print("N must be a number")
     sys.exit(1)
+
 
 def SIZE_ERROR():
     print("N must be at least 4")
     sys.exit(1)
 
+
 def IS_SAFE(BOARD, ROW, COL):
-    for I in range(COL):
-        if BOARD[ROW][I] == 1:
+    for i in range(COL):
+        if BOARD[ROW][i] == 1:
             return False
 
-    for I, J in zip(range(ROW, -1, -1), range(COL, -1, -1)):
-        if BOARD[I][J] == 1:
+    for i, j in zip(range(ROW, -1, -1), range(COL, -1, -1)):
+        if BOARD[i][j] == 1:
             return False
 
-    for I, J in zip(range(ROW, len(BOARD), 1), range(COL, -1, -1)):
-        if BOARD[I][J] == 1:
+    for i, j in zip(range(ROW, len(BOARD), 1), range(COL, -1, -1)):
+        if BOARD[i][j] == 1:
             return False
 
     return True
+
 
 def SOLVE_N_QUEENS(BOARD, COL):
     if COL >= len(BOARD):
         PRINT_SOLUTION(BOARD)
         return True
 
-    RES = False
-    for I in range(len(BOARD)):
-        if IS_SAFE(BOARD, I, COL):
-            BOARD[I][COL] = 1
-            RES = SOLVE_N_QUEENS(BOARD, COL + 1) or RES
-            BOARD[I][COL] = 0
+    res = False
+    for i in range(len(BOARD)):
+        if IS_SAFE(BOARD, i, COL):
+            BOARD[i][COL] = 1
+            res = SOLVE_N_QUEENS(BOARD, COL + 1) or res
+            BOARD[i][COL] = 0
 
-    return RES
+    return res
+
 
 def PRINT_SOLUTION(BOARD):
-    SOLUTION = []
-    for I in range(len(BOARD)):
-        for J in range(len(BOARD)):
-            if BOARD[I][J] == 1:
-                SOLUTION.append([I, J])
-    print(SOLUTION)
+    solution = []
+    for i in range(len(BOARD)):
+        for j in range(len(BOARD)):
+            if BOARD[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
 
 def MAIN():
     if len(sys.argv) != 2:
@@ -67,6 +73,7 @@ def MAIN():
     BOARD = [[0 for _ in range(N)] for _ in range(N)]
     if not SOLVE_N_QUEENS(BOARD, 0):
         pass
+
 
 if __name__ == "__main__":
     MAIN()
